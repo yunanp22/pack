79e27285a07c05b3fd90389eb41cc7dfe73964e6
Leebumju <dlqkawns@naver.com>
2022-11-18 20:45:04 +0900

    Initial Commit

---
+++ pack/packApp.swift
@@ -0,0 +1,17 @@
+//
+//  packApp.swift
+//  pack
+//
+//  Created by 이범준 on 11/18/22.
+//
+
+import SwiftUI
+
+@main
+struct packApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
